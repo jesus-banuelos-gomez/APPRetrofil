e79540f88614dcffa0caecf97afe41a56cf51710
jesus bañuelos <jesusbg1225dic@gmail.com>
2022-05-21 16:45:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appretrofil/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.appretrofil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appretrofil.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.Query
+import retrofit2.http.QueryName
+import kotlin.coroutines.coroutineContext
+
+class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+
+    }
+
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(query: String){
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val pupples:DogsResponse? = call.body()
+            if (call.isSuccessful){
+
+            }else{
+
+            }
+        }
+    }
+}
